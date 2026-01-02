f59db321179e75d2388800dd079722bc46a48eb5
Aotpy <parasaotpy@gmail.com>
2026-01-03 00:27:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-U = 'Aotpy/tobi'
-C = 'obitostuffs / obitoapi'
-P = 'https://aotpy.vercel.app/'
+import time
+GREEN = "\033[1;92m"
+RESET = "\033[0m"
+
+print(GREEN + "This Tool and enc by - @Aotpy | Channel : ObitoStuffs / @obitoapi" + RESET)
+
+time.sleep(3)
